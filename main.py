92cb63d364f8c0c659850b39e8f747fe5d1386df
Diego Cali <110750625+DiegoCali@users.noreply.github.com>
2023-04-24 07:18:28 -0600

    Fixed Bugs
    I fixed some bugs from the latest version and also added a function to check if the graph is connected

--- main.py
+++ main.py
@@ -25,35 +25,52 @@ def dijkstra_shortest_path(graph, start, end, top_n):
     return top_paths
 
 
+def is_connected(graph):
+    visited = set()
+
+    def dfs(node):
+        visited.add(node)
+        for neighbor in graph[node]:
+            if neighbor not in visited:
+                dfs(neighbor)
+    initial_node = list(graph.keys())[0]
+    dfs(initial_node)
+    return len(visited) == len(graph)
+
+
 def dijkstra():
     graph = Graph(list_nodes.list_nodes)
     new_graph = graph.create_graph()
     name_init_node = simpledialog.askstring("Initial Node",
                                             "Please write the name of the initial node", parent=window)
-    is_in_graph = list_nodes.search_node_by_name(name_init_node)
-    if name_init_node is not None and is_in_graph:
-        name_end_node = simpledialog.askstring("End Node",
-                                               "Please write the name of the last node", parent=window)
-        also_in_graph = list_nodes.search_node_by_name(name_end_node)
-        if name_end_node is not None and also_in_graph:
-            path = dijkstra_shortest_path(new_graph, name_init_node.upper(), name_end_node.upper(), 3)
-            counter, length = 0, len(path[0])
-            first_path = path[0]
-            list_edges.reinit_edges(canvas)
-            for paths in path:
-                global_path.append(paths)
-            while counter < length - 1:
-                name = first_path[counter] + first_path[counter + 1]
-                list_edges.paint_edge(name, canvas)
-                counter = counter + 1
-            for node in list_nodes.list_nodes:  # we repaint the nodes so the edges are left behind
-                node.paint_node(canvas)
-                node.change_color("yellow", canvas)            
-            print(global_path)
+    if name_init_node is not None and is_connected(new_graph):
+        is_in_graph = list_nodes.search_node_by_name(name_init_node)
+        global_path.clear()
+        if name_init_node is not None and is_in_graph:
+            name_end_node = simpledialog.askstring("End Node",
+                                                   "Please write the name of the last node", parent=window)
+            also_in_graph = list_nodes.search_node_by_name(name_end_node)
+            if name_end_node is not None and also_in_graph:
+                path = dijkstra_shortest_path(new_graph, name_init_node.upper(), name_end_node.upper(), 3)
+                counter, length = 0, len(path[0])
+                first_path = path[0]
+                list_edges.reinit_edges(canvas)
+                for paths in path:
+                    global_path.append(paths)
+                while counter < length - 1:
+                    name = first_path[counter] + first_path[counter + 1]
+                    list_edges.paint_edge(name, canvas)
+                    counter = counter + 1
+                for node in list_nodes.list_nodes:  # we repaint the nodes so the edges are left behind
+                    node.paint_node(canvas)
+                    node.change_color("yellow", canvas)
+            else:
+                msg.showinfo(title="Error", message=f"There is no node named {name_end_node}")
         else:
-            msg.showinfo(title="Error", message=f"There is no node named {name_end_node}")
+            msg.showinfo(title="Error", message=f"There is no node named {name_init_node}")
     else:
-        msg.showinfo(title="Error", message=f"There is no node named {name_init_node}")
+        if not is_connected(new_graph):
+            msg.showinfo(title="Error", message="The graph is not connected")
 
 
 def create_node(event):
